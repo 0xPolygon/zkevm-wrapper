0b2416a15765ef3ccec5a5fc50a61e02ac610c39
qedk <1994constant@gmail.com>
2023-07-07 15:43:50 +0400

    feat: implement additional tests

--- test/ZkEVMWrapper.t.sol
+++ test/ZkEVMWrapper.t.sol
@@ -27,33 +27,39 @@ contract ZkEVMWrapperTest is Test {
         external
         payable
     {
-        vm.assume(user != address(0));
+        vm.assume(user != address(0) && etherAmount != 0);
         vm.deal(user, etherAmount);
-        console.log(user.balance);
         token.mint(user, tokenAmount);
         vm.startPrank(user);
         token.approve(address(wrapper), tokenAmount);
         wrapper.deposit{value: etherAmount}(IERC20(token), tokenAmount, destination);
+        assertEq(token.balanceOf(address(bridge)), tokenAmount);
+        assertEq(address(bridge).balance, etherAmount);
     }
 
-    function testDepositPermit(uint256 privKey, uint256 tokenAmount, uint256 etherAmount, address destination)
-        external
-        payable
-    {
+    function testDepositPermit(
+        uint256 privKey,
+        uint256 tokenAmount,
+        uint256 etherAmount,
+        address destination,
+        uint256 deadline
+    ) external payable {
+        // privkey value must be lower than secp256k1 curve order
         vm.assume(
             privKey != 0 && privKey < 115792089237316195423570985008687907852837564279074904382605163141518161494337
         );
         address user = vm.addr(privKey);
-        vm.assume(user != address(0));
+        vm.assume(user != address(0) && deadline > 0 && etherAmount != 0);
         vm.deal(user, etherAmount);
         console.log(user.balance);
         token.mint(user, tokenAmount);
         vm.startPrank(user);
-        uint256 deadline = 1 minutes;
         SigUtils.Permit memory permit =
             SigUtils.Permit({owner: user, spender: address(wrapper), value: tokenAmount, nonce: 0, deadline: deadline});
         bytes32 digest = sigUtils.getTypedDataHash(permit);
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(privKey, digest);
         wrapper.deposit{value: etherAmount}(IERC20(token), tokenAmount, destination, deadline, v, r, s);
+        assertEq(token.balanceOf(address(bridge)), tokenAmount);
+        assertEq(address(bridge).balance, etherAmount);
     }
 }
