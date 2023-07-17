4d626a16228790f522d16f65edbb11f945ffdd73
qedk <1994constant@gmail.com>
2023-07-18 01:16:34 +0400

    feat: add deployment script

---
+++ script/ZkEVMWrapper.mainnet.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import {ZkEVMWrapper} from "src/ZkEVMWrapper.sol";
+import {IZkEVMBridge} from "src/IZkEVMBridge.sol";
+
+contract ZkEVMWrapperDeploy is Script {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy ZkEVMWrapper
+        ZkEVMWrapper wrapper = new ZkEVMWrapper(IZkEVMBridge(0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe));
+
+        vm.stopBroadcast();
+    }
+}
