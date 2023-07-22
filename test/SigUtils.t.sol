24d3bef64d706e9aae37d91d617408f892b006bc
qedk <1994constant@gmail.com>
2023-07-23 00:38:00 +0400

    feat: add implementation and tests for Dai

--- test/SigUtils.t.sol
+++ test/SigUtils.t.sol
@@ -3,13 +3,16 @@ pragma solidity 0.8.20;
 
 contract SigUtils {
     bytes32 internal DOMAIN_SEPARATOR;
+    bytes32 internal DAI_DOMAIN_SEPARATOR;
 
-    constructor(bytes32 _DOMAIN_SEPARATOR) {
+    constructor(bytes32 _DOMAIN_SEPARATOR, bytes32 _DAI_DOMAIN_SEPARATOR) {
         DOMAIN_SEPARATOR = _DOMAIN_SEPARATOR;
+        DAI_DOMAIN_SEPARATOR = _DAI_DOMAIN_SEPARATOR;
     }
 
     // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
     bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+    bytes32 public constant DAI_PERMIT_TYPEHASH = 0xea2aa0a1be11a07ed86d755c93467f4f82362b452371d1ba94d1715123511acb;
 
     struct Permit {
         address owner;
@@ -19,15 +22,35 @@ contract SigUtils {
         uint256 deadline;
     }
 
-    // computes the hash of a permit
+    struct DaiPermit {
+        address holder;
+        address spender;
+        uint256 nonce;
+        uint256 expiry;
+        bool allowed;
+    }
+
+    // computes the hash of an EIP-2612 permit
     function getStructHash(Permit memory _permit) internal pure returns (bytes32) {
         return keccak256(
             abi.encode(PERMIT_TYPEHASH, _permit.owner, _permit.spender, _permit.value, _permit.nonce, _permit.deadline)
         );
     }
 
+    // computes the hash of a Dai permit
+    function getDaiStructHash(DaiPermit memory _permit) internal pure returns (bytes32) {
+        return keccak256(
+            abi.encode(DAI_PERMIT_TYPEHASH, _permit.holder, _permit.spender, _permit.nonce, _permit.expiry, _permit.allowed)
+        );
+    }
+
     // computes the hash of the fully encoded EIP-712 message for the domain, which can be used to recover the signer
     function getTypedDataHash(Permit memory _permit) public view returns (bytes32) {
         return keccak256(abi.encodePacked("\x19\x01", DOMAIN_SEPARATOR, getStructHash(_permit)));
+    }
+
+    // computes the hash of the fully encoded EIP-712 message for the domain, which can be used to recover the signer
+    function getDaiTypedDataHash(DaiPermit memory _permit) public view returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19\x01", DAI_DOMAIN_SEPARATOR, getDaiStructHash(_permit)));
     }
 }
--- test/ZkEVMWrapper.t.sol
+++ test/ZkEVMWrapper.t.sol
@@ -2,23 +2,27 @@
 pragma solidity 0.8.20;
 
 import {MockERC20} from "src/mocks/MockERC20.sol";
+import {MockDai} from "src/mocks/MockDai.sol";
 import {MockZkEVMBridge} from "src/mocks/MockZkEVMBridge.sol";
 import {ZkEVMWrapper} from "src/ZkEVMWrapper.sol";
 import {IZkEVMBridge} from "src/interfaces/IZkEVMBridge.sol";
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {IERC20Permit} from "openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import {IDai} from "src/interfaces/IDai.sol";
 import {SigUtils} from "./SigUtils.t.sol";
 import "forge-std/Test.sol";
 
 contract ZkEVMWrapperTest is Test {
     MockERC20 public token;
+    MockDai public dai;
     ZkEVMWrapper public wrapper;
     MockZkEVMBridge public bridge;
     SigUtils public sigUtils;
 
     function setUp() external {
         token = new MockERC20();
-        sigUtils = new SigUtils(IERC20Permit(token).DOMAIN_SEPARATOR());
+        dai = new MockDai(block.chainid);
+        sigUtils = new SigUtils(IERC20Permit(token).DOMAIN_SEPARATOR(), dai.DOMAIN_SEPARATOR());
         bridge = new MockZkEVMBridge();
         wrapper = new ZkEVMWrapper(IZkEVMBridge(address(bridge)));
     }
@@ -60,6 +64,31 @@ contract ZkEVMWrapperTest is Test {
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(privKey, digest);
         wrapper.deposit{value: etherAmount}(IERC20(token), tokenAmount, destination, deadline, v, r, s);
         assertEq(token.balanceOf(address(bridge)), tokenAmount);
+        assertEq(address(bridge).balance, etherAmount);
+    }
+
+    function testDepositDaiPermit(
+        uint256 privKey,
+        uint256 tokenAmount,
+        uint256 etherAmount,
+        address destination,
+        uint256 expiry
+    ) external payable {
+        // privkey value must be lower than secp256k1 curve order
+        vm.assume(
+            privKey != 0 && privKey < 115792089237316195423570985008687907852837564279074904382605163141518161494337
+        );
+        address user = vm.addr(privKey);
+        vm.assume(user != address(0) && expiry > 0 && etherAmount != 0);
+        vm.deal(user, etherAmount);
+        dai.mint(user, tokenAmount);
+        vm.startPrank(user);
+        SigUtils.DaiPermit memory permit =
+            SigUtils.DaiPermit({holder: user, spender: address(wrapper), nonce: 0, expiry: expiry, allowed: true});
+        bytes32 digest = sigUtils.getDaiTypedDataHash(permit);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privKey, digest);
+        wrapper.deposit{value: etherAmount}(IDai(address(dai)), tokenAmount, destination, 0, expiry, true, v, r, s);
+        assertEq(dai.balanceOf(address(bridge)), tokenAmount);
         assertEq(address(bridge).balance, etherAmount);
     }
 }
